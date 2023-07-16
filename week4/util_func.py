60e8d15192d81879a01f2771be11e92b51aea50f
TanMoon20 <tanseemoon@gmail.com>
2023-07-16 20:30:06 +0800

    added week4

---
+++ week4/util_func.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+
+def show_img(window_name, img, adjust=False):
+    """3 arguments: window name, source images, boolean to adjust to screen size"""
+    if adjust:
+        cv.namedWindow(window_name, cv.WINDOW_NORMAL)
+    else:
+        cv.namedWindow(window_name)
+
+    cv.imshow(window_name, img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
